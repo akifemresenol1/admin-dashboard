fe2d1a5f1767790668354fcdbf1df061ce6b02d9
Akif Emre Şenol <akifffemreee@gmail.com>
2024-03-12 01:21:30 +0300

    feat: Added Card, Chart, Rightbar, and Transactions sections to the dashboard
    Implemented Card, Chart, Rightbar, and Transactions sections to the dashboard. General CSS properties have been applied to the dashboard. Completed styling for Card and Transactions sections.

---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+import styles from "./transactions.module.css";
+
+const Transactions = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Pending</span>
+            </td>
+            <td>12.03.2024</td>
+            <td>$4.300</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Pending
+              </span>
+            </td>
+            <td>12.03.2024</td>
+            <td>$4.300</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>12.03.2024</td>
+            <td>$4.300</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Pending</span>
+            </td>
+            <td>12.03.2024</td>
+            <td>$4.300</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Transactions;
